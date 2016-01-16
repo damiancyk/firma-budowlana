54403da46df1cb02d3f5ac135b046219f5e8c9e3
damian.pieta <damiancyk@gmail.com>
2016-01-17 00:07:57 +0100

    zmieniona ikonka

--- assets/page/header.php
+++ assets/page/header.php
@@ -4,12 +4,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <meta name="description" content="" />
 <meta name="author" content="" />
-<link rel="shortcut icon" type="image/x-icon" href="assets/img/poczta_ikona.png">
+<link rel="shortcut icon" type="image/x-icon" href="assets/img/icon.png">
 
 <!--[if IE]>
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <![endif]-->
-<title>LIGHT WAVE</title>
+<title>PAWEŁ KRAWIEC - FIRMA BUDOWLANA</title>
 <!-- BOOTSTRAP CORE CSS -->
 <link href="assets/css/bootstrap.css" rel="stylesheet" />
 <!-- ION ICONS STYLES -->
